ba368c2e8087afe2af82de03ca77d23d0cd1b5da
Pixel Lifetime <26673040+PixelLifetime@users.noreply.github.com>
2022-07-31 02:20:54 +0300

    Add functions to scene operator.

--- Runtime/SceneOperator.cs
+++ Runtime/SceneOperator.cs
@@ -56,8 +56,14 @@ public class SceneOperator : MonoBehaviour
 	public void LoadScene(SceneReference sceneReference, LoadSceneMode loadSceneMode) => SceneController._Instance.LoadScene(sceneReference: sceneReference, loadSceneMode: loadSceneMode);
 	public void LoadScene(SceneReference sceneReference) => SceneController._Instance.LoadScene(sceneReference: sceneReference);
 
-	public void LoadScene(SceneReferenceCollection sceneReferenceCollection, LoadSceneMode loadSceneMode) => SceneController._Instance.LoadSceneReferenceCollection(sceneReferenceCollection: sceneReferenceCollection, loadSceneMode: loadSceneMode);
-	public void LoadScene(SceneReferenceCollection sceneReferenceCollection) => SceneController._Instance.LoadSceneReferenceCollection(sceneReferenceCollection: sceneReferenceCollection);
+	public void LoadSceneReferenceCollection(SceneReferenceCollection sceneReferenceCollection, LoadSceneMode loadSceneMode) => SceneController._Instance.LoadSceneReferenceCollection(sceneReferenceCollection: sceneReferenceCollection, loadSceneMode: loadSceneMode);
+	public void LoadSceneReferenceCollection(SceneReferenceCollection sceneReferenceCollection) => SceneController._Instance.LoadSceneReferenceCollection(sceneReferenceCollection: sceneReferenceCollection);
+
+	public void LoadPreviousSceneReferenceCollection(LoadSceneMode loadSceneMode) => SceneController._Instance.LoadPreviousSceneReferenceCollection(loadSceneMode: loadSceneMode);
+	public void LoadPreviousSceneReferenceCollection() => SceneController._Instance.LoadPreviousSceneReferenceCollection();
+
+	public void LoadNextSceneReferenceCollection(LoadSceneMode loadSceneMode) => SceneController._Instance.LoadNextSceneReferenceCollection(loadSceneMode: loadSceneMode);
+	public void LoadNextSceneReferenceCollection() => SceneController._Instance.LoadNextSceneReferenceCollection();
 	#endregion
 
 	public void Exit() => SceneController._Instance.Exit();
